f4fbce7c586f33c3cb7ef6dd48adeddf8cd57558
RJS66 <rjs.github.com>
2020-06-27 20:51:34 -0400

    sync and not sync mutable array

---
+++ java8/src/main/java/concurrency/PrintTask.java
@@ -0,0 +1,33 @@
+package concurrency;
+
+import java.security.SecureRandom;
+
+public class PrintTask implements Runnable {
+
+	private final static SecureRandom generator = new SecureRandom();
+	private final int sleepTime; // random sleep time for thread
+	private final String taskName; // name of task
+	
+	// constructor
+	public PrintTask(String mtaskName) {
+		taskName = mtaskName;
+		
+		// pick random sleep time between 0 and 5 seconds
+		sleepTime = generator.nextInt(5000);
+	}
+	
+	// method run contains the code that a thread will execute
+	public void run() {
+		try { // put thread to sleep for sleepTime amount of time
+			System.out.printf("%s going to sleep for %d milliseconds.%n",
+					taskName, sleepTime);
+			Thread.sleep(sleepTime); // put thread to sleep
+		} catch (InterruptedException exception) {
+			exception.printStackTrace();
+			Thread.currentThread().interrupt(); // re-interrupt the thread
+		}
+		
+		// print task name
+		System.out.printf("%s done sleeping%n", taskName);
+	}
+}
---
+++ java8/src/main/java/concurrency/TaskExecutor.java
@@ -0,0 +1,31 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TaskExecutor {
+
+	public static void main(String[] args) {
+		
+		// create and name each runnable
+		// these are runnable objects that can be executed as threads
+		PrintTask task1 = new PrintTask("task1");
+		PrintTask task2 = new PrintTask("task2");
+		PrintTask task3 = new PrintTask("task3");
+		
+		System.out.println("Starting Executor");
+		
+		// create ExecutorService to manage threads
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		
+		// start the three PrintTasks
+		executorService.execute(task1);
+		executorService.execute(task2);
+		executorService.execute(task3);
+		
+		// shut down ExecutorService -- it decides when to shut down threads
+		executorService.shutdown();
+		
+		System.out.printf("Tasks started, main ends.%n%n");
+	}
+}
